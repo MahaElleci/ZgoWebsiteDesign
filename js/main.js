8c8d05547bb5260d91f148c1db9305fa086b9116
MahaElleci <maha.ehab.elleci@gmail.com>
2017-12-28 04:01:24 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$("#marketing-link").click(function(){
+      $("#marketing-details").slideToggle(); 
+      $("#page-wrapper").css("background-color", "white");
+  }); 
+
+$("#accounting-link").click(function(){
+      $("#accounting-details").slideToggle(); 
+      $("#page-wrapper").css("background-color", "white");
+  }); 
+
+$("#logistics-link").click(function(){
+      $("#logistics-details").slideToggle(); 
+      $("#page-wrapper").css("background-color", "white");
+  }); 
+
+
+});
